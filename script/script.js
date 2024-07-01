7f959cb1c1782500a034a24cbf155d3aee6ad4e2
Vxrcel <skyeparagon@gmail.com>
2024-07-01 14:28:40 +0100

    welcome

--- script/script.js
+++ script/script.js
@@ -1,10 +1,17 @@
+
+const welcome_div = document.querySelector(".welcome");
+(function hideWelcome() {
+    const isWelcomed = localStorage.getItem('welcome');
+    if (isWelcomed === "true") {
+        welcome_div.style.display = "none";
+        return
+    }
+})()
 // Remember user's theme preference
 // Doesn't affect anything else
 document.addEventListener('DOMContentLoaded', function () {
     const checkbox = document.querySelector('[data-testid="dark-mode-checkbox"]');
-
     checkbox.checked = localStorage.getItem('darkMode') === 'true';
-
     checkbox.addEventListener('change', function (event) {
         localStorage.setItem('darkMode', event.currentTarget.checked);
     });
@@ -12,6 +19,13 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 const dataDay = document.querySelector("[data-testid='currentDay']");
 const dataTime = document.querySelector("[data-testid='currentTimeUTC']");
+
+
+window.addEventListener("load", function () {
+    welcome_div.classList.add("hide-welcome");
+    this.localStorage.setItem("welcome", "true");
+});
+
 
 console.log(dataDay)
 function updateUTCTime() {
